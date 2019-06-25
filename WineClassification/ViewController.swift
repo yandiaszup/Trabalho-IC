11ccc27f86667ac70bab1ab5aafcafd20ffca7f0
Yan Dias <yan.dias@zup.com.br>
2019-06-24 22:43:05 -0300

    Initial Commit

---
+++ WineClassification/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WineClassification
+//
+//  Created by Yan Dias on 24/06/19.
+//  Copyright © 2019 Yan lucas damasceno dias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
